147ea06e8e9c4017ce307f11d6ceebec724ed2c7
tosin <tosin.sonuyi@gmail.com>
2023-10-30 17:52:28 -0400

    add spring animation to modulo grouping component

--- src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
+++ src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
@@ -1,30 +1,29 @@
-'use client';
-import React from 'react';
-import clsx from 'clsx';
+"use client";
+import React from "react";
+import clsx from "clsx";
 
-import { range } from '@/utils';
-import Card from '@/components/Card';
-import SliderControl from '@/components/SliderControl';
+import { range } from "@/utils";
+import Card from "@/components/Card";
+import SliderControl from "@/components/SliderControl";
 
-import Equation from './Equation';
-import styles from './DivisionGroupsDemo.module.css';
+import Equation from "./Equation";
+import styles from "./DivisionGroupsDemo.module.css";
+
+import { motion, LayoutGroup } from "framer-motion";
 
 function DivisionGroupsDemo({
   numOfItems = 12,
   initialNumOfGroups = 1,
   includeRemainderArea,
 }) {
-  const [numOfGroups, setNumOfGroups] = React.useState(
-    initialNumOfGroups
-  );
+  const id = React.useId();
+  const [numOfGroups, setNumOfGroups] = React.useState(initialNumOfGroups);
 
-  const numOfItemsPerGroup = Math.floor(
-    numOfItems / numOfGroups
-  );
+  const numOfItemsPerGroup = Math.floor(numOfItems / numOfGroups);
 
-  const remainder = includeRemainderArea
-    ? numOfItems % numOfGroups
-    : null;
+  const remainder = includeRemainderArea ? numOfItems % numOfGroups : null;
+
+  let ballCountIndex = 0;
 
   // When we're splitting into 1-3 groups, display side-by-side
   // columns. When we get to 4, it should switch to a 2x2 grid.
@@ -34,8 +33,8 @@ function DivisionGroupsDemo({
           gridTemplateColumns: `repeat(${numOfGroups}, 1fr)`,
         }
       : {
-          gridTemplateColumns: '1fr 1fr',
-          gridTemplateRows: '1fr 1fr',
+          gridTemplateColumns: "1fr 1fr",
+          gridTemplateRows: "1fr 1fr",
         };
 
   return (
@@ -48,45 +47,56 @@ function DivisionGroupsDemo({
           min={1}
           max={4}
           value={numOfGroups}
-          onChange={(ev) =>
-            setNumOfGroups(Number(ev.target.value))
-          }
+          onChange={(ev) => setNumOfGroups(Number(ev.target.value))}
         />
       </header>
 
-      <div className={styles.demoWrapper}>
-        <div
-          className={clsx(styles.demoArea)}
-          style={gridStructure}
-        >
-          {range(numOfGroups).map((groupIndex) => (
-            <div key={groupIndex} className={styles.group}>
-              {range(numOfItemsPerGroup).map((index) => {
-                return (
-                  <div
-                    key={index}
-                    className={styles.item}
-                  />
-                );
-              })}
-            </div>
-          ))}
-        </div>
-      </div>
+      <LayoutGroup>
+        <div className={styles.demoWrapper}>
+          <div className={clsx(styles.demoArea)} style={gridStructure}>
+            {range(numOfGroups).map((groupIndex) => (
+              <div key={groupIndex} className={styles.group}>
+                {range(numOfItemsPerGroup).map((index) => {
+                  ballCountIndex += 1;
+                  const ballLayoutId = `${id}-${ballCountIndex}`;
 
-      {includeRemainderArea && (
-        <div className={styles.remainderArea}>
-          <p className={styles.remainderHeading}>
-            Remainder Area
-          </p>
-
-          {range(remainder).map((index) => {
-            return (
-              <div key={index} className={styles.item} />
-            );
-          })}
+                  return (
+                    <motion.div
+                      transition={{ type: "spring", stiffness: 15, damping: 5 }}
+                      layoutId={ballLayoutId}
+                      key={ballLayoutId}
+                      className={styles.item}
+                    ></motion.div>
+                  );
+                })}
+              </div>
+            ))}
+          </div>
         </div>
-      )}
+
+        {includeRemainderArea && (
+          <div className={styles.remainderArea}>
+            <p className={styles.remainderHeading}>Remainder Area</p>
+
+            {range(remainder).map((index) => {
+              // ballCountIndex += 1;
+              // use this to count in reverse to facilate the balls moving to the front
+              const remainderBallCountIndex =
+                ballCountIndex + remainder - index;
+              // console.log("rmBCI - index: ", rmBCI - index);
+              const ballLayoutId = `${id}-${remainderBallCountIndex}`;
+              return (
+                <motion.div
+                  layoutId={ballLayoutId}
+                  transition={{ type: "spring", stiffness: 15, damping: 5 }}
+                  key={ballLayoutId}
+                  className={styles.item}
+                ></motion.div>
+              );
+            })}
+          </div>
+        )}
+      </LayoutGroup>
 
       <Equation
         dividend={numOfItems}
