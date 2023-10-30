938ebb54875c536ff843d20b4cde0732c5522296
tosin <tosin.sonuyi@gmail.com>
2023-10-30 12:41:48 -0400

    use metadata api, dynamic list of home page posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import BlogSummaryCard from '@/components/BlogSummaryCard';
+import BlogSummaryCard from "@/components/BlogSummaryCard";
+import { getBlogPostList } from "@/helpers/file-helpers";
+import styles from "./homepage.module.css";
+import { BLOG_DESCRIPTION, BLOG_TITLE } from "@/constants";
 
-import styles from './homepage.module.css';
+export async function generateMetadata() {
+  return {
+    title: BLOG_TITLE,
+    description: BLOG_DESCRIPTION,
+  };
+}
 
-function Home() {
+async function Home() {
+  const blogList = await getBlogPostList();
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
 
       {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      {blogList.map((blogData) => {
+        return (
+          <BlogSummaryCard
+            key={blogData.slug}
+            slug={blogData.slug}
+            title={blogData.title}
+            abstract={blogData.abstract}
+            publishedOn={blogData.publishedOn}
+          />
+        );
+      })}
     </div>
   );
 }
