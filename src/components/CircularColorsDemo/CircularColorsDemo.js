3670c4b4a5675daf0acb8f9ba08ee105cf3d7bc4
tosin <tosin.sonuyi@gmail.com>
2023-10-30 17:50:32 -0400

    add timer for modulo componenet and framer animations

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,79 +1,93 @@
-import React from 'react';
-import clsx from 'clsx';
-import {
-  Play,
-  Pause,
-  RotateCcw,
-} from 'react-feather';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { Play, Pause, RotateCcw } from "react-feather";
 
-import Card from '@/components/Card';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import Card from "@/components/Card";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
-import styles from './CircularColorsDemo.module.css';
+import styles from "./CircularColorsDemo.module.css";
 
+import { motion } from "framer-motion";
 const COLORS = [
-  { label: 'red', value: 'hsl(348deg 100% 60%)' },
-  { label: 'yellow', value: 'hsl(50deg 100% 55%)' },
-  { label: 'blue', value: 'hsl(235deg 100% 65%)' },
+  { label: "red", value: "hsl(348deg 100% 60%)" },
+  { label: "yellow", value: "hsl(50deg 100% 55%)" },
+  { label: "blue", value: "hsl(235deg 100% 65%)" },
 ];
 
 function CircularColorsDemo() {
-  // TODO: This value should increase by 1 every second:
-  const timeElapsed = 0;
+  const [timeElapsed, setTimeElapsed] = React.useState(0);
+  const [isPlaying, setIsPlaying] = React.useState(true);
 
-  // TODO: This value should cycle through the colors in the
-  // COLORS array:
-  const selectedColor = COLORS[0];
+  const selectedColor = COLORS[timeElapsed % COLORS.length];
+
+  const timer = React.useRef();
+
+  React.useEffect(() => {
+    if (!isPlaying && timer.current) {
+      console.log("clearing timer! ", timer.current);
+      window.clearInterval(timer.current);
+      timer.current = null;
+      return;
+    }
+    timer.current = window.setInterval(() => {
+      setTimeElapsed((timeElapsed) => timeElapsed + 1);
+    }, 1000);
+    return () => window.clearInterval(timer.current);
+  }, [isPlaying]);
+
+  function switchPlayingStatus() {
+    setIsPlaying(!isPlaying);
+  }
 
   return (
     <Card as="section" className={styles.wrapper}>
       <ul className={styles.colorsWrapper}>
         {COLORS.map((color, index) => {
-          const isSelected =
-            color.value === selectedColor.value;
+          const isSelected = color.value === selectedColor.value;
 
           return (
-            <li
-              className={styles.color}
-              key={index}
-            >
+            <li className={styles.color} key={index}>
               {isSelected && (
-                <div
-                  className={
-                    styles.selectedColorOutline
-                  }
+                <motion.div
+                  initial={true}
+                  layoutId="background-backdrop"
+                  className={styles.selectedColorOutline}
                 />
               )}
               <div
                 className={clsx(
                   styles.colorBox,
-                  isSelected &&
-                    styles.selectedColorBox
+                  isSelected && styles.selectedColorBox
                 )}
                 style={{
                   backgroundColor: color.value,
                 }}
               >
-                <VisuallyHidden>
-                  {color.label}
-                </VisuallyHidden>
+                <VisuallyHidden>{color.label}</VisuallyHidden>
               </div>
             </li>
           );
         })}
       </ul>
-
       <div className={styles.timeWrapper}>
         <dl className={styles.timeDisplay}>
           <dt>Time Elapsed</dt>
           <dd>{timeElapsed}</dd>
         </dl>
         <div className={styles.actions}>
-          <button>
-            <Play />
-            <VisuallyHidden>Play</VisuallyHidden>
-          </button>
-          <button>
+          {!isPlaying ? (
+            <button onClick={switchPlayingStatus}>
+              <Play />
+              <VisuallyHidden>Play</VisuallyHidden>
+            </button>
+          ) : (
+            <button onClick={switchPlayingStatus}>
+              <Pause />
+              <VisuallyHidden>Pause</VisuallyHidden>
+            </button>
+          )}
+          <button onClick={() => setTimeElapsed(0)}>
             <RotateCcw />
             <VisuallyHidden>Reset</VisuallyHidden>
           </button>
